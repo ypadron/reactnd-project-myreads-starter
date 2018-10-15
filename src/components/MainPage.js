a8e2dba44e846ce8c98bb0ac49743cf9a9d589df
ypadron <ypadron82@gmail.com>
2018-10-15 17:11:52 -0400

    refactor: Add title attr to BookShelf component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,7 +11,9 @@ class MainPage extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf />
+            <BookShelf title='Currently Reading' />
+            <BookShelf title='Want To Read'/>
+            <BookShelf title='Read'/>
           </div>
         </div>
         <div className="open-search">
