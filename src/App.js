b964aa7bc0e91a098c9f83afd340d89dff0c53b0
ypadron <ypadron82@gmail.com>
2018-10-16 00:59:53 -0400

    refactor: Add state and componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React from 'react'
 
 import MainPage from './components/MainPage.js';
 import SearchPage from './components/SearchPage.js';
-// import * as BooksAPI from './BooksAPI'
+
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
 
+  componentDidMount() {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books })
+      })
+    }
+
   render() {
+    /*console.log(this.state.books);*/
     return (
       <div className="app" >
-        <MainPage />
+        <MainPage
+          books={this.state.books}
+        />
       </div>
     )
   }
