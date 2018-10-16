723f56685431dd69b0234ee61f3e4de21ea48b87
ypadron <ypadron82@gmail.com>
2018-10-16 01:17:10 -0400

    refactor: Merge to MainPage.js - not working

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Book from './Book.js';
+// import Book from './Book.js';
+// import * as BooksAPI from '../BooksAPI'
 
+/*
 class BookShelf extends React.Component {
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+            {
+              this.props.books
+                .filter(book => book.shelf = 'currentlyReading')
+                .map(book => (
+                  <li >
+                    <Book />
+                  </li>
+                ))
+            }
           </ol>
         </div>
       </div>
+
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">'Want to Read'</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {
+              this.props.books
+                .filter(book => book.shelf = 'wantToRead')
+                .map(book => (
+                  <li >
+                    <Book />
+                  </li>
+                ))
+            }
+          </ol>
+        </div>
+      </div>
+
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">'Read'</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {
+              this.props.books
+                .filter(book => book.shelf = 'read')
+                .map(book => (
+                  <li >
+                    <Book />
+                  </li>
+                ))
+            }
+          </ol>
+        </div>
+      </div>
+*/
     );
   }
 }
