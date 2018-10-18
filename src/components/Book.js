e5cd2ca24d9aa8da90e46abaff5009d1892b8bec
ypadron <ypadron82@gmail.com>
2018-10-17 20:07:50 -0400

    feat: Define displayedThumbnail, add onClick event

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,12 +3,20 @@ import React from 'react';
 
 class Book extends React.Component {
   render() {
+    let displayedThumbnail = this.props.book.imageLinks ?
+    this.props.book.imageLinks.thumbnail : '';
+
     return  (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${displayedThumbnail}")` }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select
+              onChange={(event) => this.props.moveShelf(
+                this.props.book, event.target.value
+              )}
+              value={this.props.currentShelf}
+            >
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
