1d90ef70f67ea4c070672d2318f0d3d8d68e029a
ypadron <ypadron82@gmail.com>
2018-10-28 23:48:10 -0400

    refactor: updateQuery method & searchedBooks state

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -12,9 +12,9 @@ class SearchPage extends React.Component {
     searchedBooks: []
   }
 
-  updateQuery = (query) => {
-  this.setState({ query: query })
-  this.updateSearchedBooks(query);
+  updateQuery = (query, props) => {
+    this.setState({ query: query})
+    this.updateSearchedBooks(query);
   }
 
   updateSearchedBooks = (query) => {
@@ -26,7 +26,7 @@ class SearchPage extends React.Component {
           this.setState({ searchedBooks: searchedBooks })
         }
       })
-    } else {
+  } else {
       this.setState({ searchedBooks: [] });
     }
   }
@@ -49,14 +49,27 @@ class SearchPage extends React.Component {
 
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.searchedBooks.map(searchedBook => (
-              <li key={searchedBook.id}>
-                <Book
-                  book={searchedBook}
-                  moveShelf={this.props.moveShelf}
-                />
-              </li>
-            ))}
+            {this.state.searchedBooks
+              .map(searchedBook => {
+                let shelf = "none";
+
+                this.props.books.map(book => (
+                  book.id === searchedBook.id ?
+                  shelf = book.shelf :
+                  ''
+                ));
+                return (
+                  <li key={searchedBook.id}>
+                    <Book
+                      book={searchedBook}
+                      // setShelf={this.props.setShelf}
+                      moveShelf={this.props.moveShelf}
+                      currentShelf={shelf}
+                    />
+                  </li>
+                );
+              })
+            }
           </ol>
         </div>
       </div>
