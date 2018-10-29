cce5802c82125889c997017b29283ff444c2a347
ypadron <ypadron82@gmail.com>
2018-10-28 23:39:14 -0400

    refactor: Update BookShelf component from MainPage

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -7,56 +7,70 @@ import React, {Component} from 'react';
 class BookShelf extends React.Component {
   render() {
     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {
-              this.props.books
-                .filter(book => book.shelf = 'currentlyReading')
-                .map(book => (
-                  <li >
-                    <Book />
-                  </li>
-                ))
-            }
-          </ol>
-        </div>
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Currently Reading</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            this.props.books
+              .filter(book => book.shelf === 'currentlyReading')
+              .map(book => (
+                <li key={book.id}>
+                  <Book
+                    book={book}
+                    setShelf={this.props.setShelf}
+                    moveShelf={this.props.moveShelf}
+                    currentShelf={book.shelf}
+                    />
+                </li>
+              ))
+          }
+        </ol>
       </div>
+    </div>
 
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">'Want to Read'</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {
-              this.props.books
-                .filter(book => book.shelf = 'wantToRead')
-                .map(book => (
-                  <li >
-                    <Book />
-                  </li>
-                ))
-            }
-          </ol>
-        </div>
-      </div>
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Want to Read</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            this.props.books
+              .filter(book => book.shelf === 'wantToRead')
+              .map(book => (
+                <li key={book.id}>
+                  <Book
+                    book={book}
+                    moveShelf={this.props.moveShelf}
+                    currentShelf={book.shelf}
 
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">'Read'</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {
-              this.props.books
-                .filter(book => book.shelf = 'read')
-                .map(book => (
-                  <li >
-                    <Book />
-                  </li>
-                ))
-            }
-          </ol>
-        </div>
+                  />
+                </li>
+              ))
+          }
+        </ol>
       </div>
+    </div>
+
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Read</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            this.props.books
+              .filter(book => book.shelf === 'read')
+              .map(book => (
+                <li key={book.id}>
+                  <Book
+                    book={book}
+                    moveShelf={this.props.moveShelf}
+                    currentShelf={book.shelf}
+                   />
+                </li>
+              ))
+          }
+        </ol>
+      </div>
+    </div>
 */
     );
   }
