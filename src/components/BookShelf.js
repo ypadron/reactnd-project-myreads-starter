b23d70e2c27292fb5dadda405eebc24c32b4ce1d
ypadron <ypadron82@gmail.com>
2018-11-05 13:39:23 -0500

    refactor: book object

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,26 +1,23 @@
 import React, {Component} from 'react';
+import Book from './Book.js';
 
-// import Book from './Book.js';
 // import * as BooksAPI from '../BooksAPI'
 
-/*
 class BookShelf extends React.Component {
   render() {
     return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
+      <h2 className="bookshelf-title">{this.props.title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {
-            this.props.books
-              .filter(book => book.shelf === 'currentlyReading')
-              .map(book => (
+            this.props.books.map(book => (
                 <li key={book.id}>
                   <Book
                     book={book}
                     // setShelf={this.props.setShelf}
-                    moveShelf={this.props.moveShelf}
-                    currentShelf="currentlyReading"//{book.shelf}
+                    updateShelf={this.updateShelf}
+                    currentShelf={book.shelf}
                     />
                 </li>
               ))
@@ -28,53 +25,6 @@ class BookShelf extends React.Component {
         </ol>
       </div>
     </div>
-
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">Want to Read</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {
-            this.props.books
-              .filter(book => book.shelf === 'wantToRead')
-              .map(book => (
-                <li key={book.id}>
-                  <Book
-                    book={book}
-                    moveShelf={this.props.moveShelf}
-                    currentShelf="wantToRead"//{book.shelf}
-
-                  />
-                </li>
-              ))
-          }
-        </ol>
-      </div>
-    </div>
-
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">Read</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {
-            this.props.books
-              .filter(book => book.shelf === 'read')
-              .map(book => (
-                <li key={book.id}>
-                  <Book
-                    book={book}
-                    moveShelf={this.props.moveShelf}
-                    currentShelf="read"//{book.shelf}
-                   />
-                </li>
-              ))
-          }
-        </ol>
-      </div>
-    </div>
-
-  </div>
-</div>
-*/
     );
   }
 }
