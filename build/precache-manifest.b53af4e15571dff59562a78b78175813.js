facb14cc1fda0802d78de091256b4d1544c533da
ypadron <ypadron82@gmail.com>
2018-11-19 15:17:44 -0500

    chore: Update package manager configs

---
+++ build/precache-manifest.b53af4e15571dff59562a78b78175813.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads-starter/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "d3f84a960ae76803b400",
+    "url": "/reactnd-project-myreads-starter/static/js/main.d3f84a96.chunk.js"
+  },
+  {
+    "revision": "2ca774e1f2877a60162f",
+    "url": "/reactnd-project-myreads-starter/static/js/1.2ca774e1.chunk.js"
+  },
+  {
+    "revision": "d3f84a960ae76803b400",
+    "url": "/reactnd-project-myreads-starter/static/css/main.9d01d2bf.chunk.css"
+  },
+  {
+    "revision": "c4d92d43c9f8421ca0c81fa5206c04a0",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/reactnd-project-myreads-starter/index.html","051e89b4170174e716a81cfed9fb96fc"],["/reactnd-project-myreads-starter/static/css/main.c71b6d9c.css","350d9572c492c0f5cdb4106cc08acb95"],["/reactnd-project-myreads-starter/static/js/main.90404602.js","d6011dfdd1bf8882348b4997c09e68d5"],["/reactnd-project-myreads-starter/static/media/add.bb9363b8.svg","bb9363b86b02777d23523851826414e0"],["/reactnd-project-myreads-starter/static/media/arrow-back.3687aa98.svg","3687aa988bec28f17ae39153db1646c3"],["/reactnd-project-myreads-starter/static/media/arrow-drop-down.78c6f53b.svg","78c6f53b2342643639fe1cbaf171bf47"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var r=new URL(e);return"/"===r.pathname.slice(-1)&&(r.pathname+=t),r.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,r,n){var a=new URL(e);return n&&a.pathname.match(n)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(r)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var r=new URL(t).pathname;return e.some(function(e){return r.match(e)})},stripIgnoredUrlParameters=function(e,r){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return r.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],r=e[1],n=new URL(t,self.location),a=createCacheKey(n,hashParamName,r,/\.\w{8}\./);return[n.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(n){return setOfCachedUrls(n).then(function(r){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!r.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return n.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var r=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!r.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,r=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),n="index.html";(e=urlsToCacheKeys.has(r))||(r=addDirectoryIndex(r,n),e=urlsToCacheKeys.has(r));var a="/reactnd-project-myreads-starter/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(r=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(r)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(r)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactnd-project-myreads-starter/precache-manifest.b53af4e15571dff59562a78b78175813.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactnd-project-myreads-starter/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
